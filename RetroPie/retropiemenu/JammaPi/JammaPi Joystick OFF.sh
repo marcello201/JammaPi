0e439a4e7271162c2ee7b7335ede45eb116ece7b
vince87 <vincenzo.bini@gmail.com>
2018-09-20 21:29:01 +0200

    Add files via upload

---
+++ RetroPie/retropiemenu/JammaPi/JammaPi Joystick OFF.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo grep '#dtparam=i2c_vc=on' /boot/config.txt > /dev/null 2>&1
+if [ $? -eq 0 ] ; then
+echo "Il driver joystick JammaPi non è attivo!"
+sleep 4
+else
+echo "Disattivo il driver joystick JammaPi!"
+
+sudo perl -p -i -e 's/i2c-dev/#i2c-dev/g' /etc/modules
+sudo perl -p -i -e 's/mk_arcade_joystick_rpi/#mk_arcade_joystick_rpi/g' /etc/modules
+
+sudo perl -p -i -e 's/dtparam=i2c_vc=on/#dtparam=i2c_vc=on/g' /boot/config.txt
+
+echo "Modifiche effettuate! Riavvio in corso!"
+sleep 5
+sudo reboot
+fi
+
+
+
+
