9e8dbf9d46366e9e7a5fffed9d85ef7b0bede92d
vince87 <vincenzo.bini@gmail.com>
2018-09-26 21:58:55 +0200

    Update and rename ScartPi Attiva.sh to Attiva ScartPi-JammaPi.sh

--- RetroPie/retropiemenu/JammaPi/Attiva ScartPi-JammaPi.sh
+++ RetroPie/retropiemenu/JammaPi/Attiva ScartPi-JammaPi.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo grep '#dtoverlay=vga666-6' /boot/config.txt > /dev/null 2>&1
 if [ $? -eq 0 ] ; then
-echo "Il driver ScartPi non Ë attivo!"
+echo "Il driver Video non √® attivo!"
 sleep 4
-echo "Attivo il driver ScartPi!"
+echo "Attivo il driver Video
 
 sudo perl -p -i -e 's/#dtoverlay=pwm-2chan,pin=18,func=2,pin2=19,func2=2/dtoverlay=pwm-2chan,pin=18,func=2,pin2=19,func2=2/g' /boot/config.txt
 sudo perl -p -i -e 's/#dtoverlay=vga666-6/dtoverlay=vga666-6/g' /boot/config.txt
@@ -18,7 +18,7 @@ echo "Modifiche effettuate! Riavvio in corso!"
 sleep 5
 sudo reboot
 else
-echo "Il driver ScartPi Ë gi‡ attivo!"
+echo "Il driver video √® gi√† attivo!"
 fi
 
 
