f9146ba76c37999e3d83e0710eca080cb61d9893
vince87 <vincenzo.bini@gmail.com>
2018-10-18 18:43:58 +0200

    Update Attiva ScartPi-JammaPi.sh

--- RetroPie/retropiemenu/JammaPi/Attiva ScartPi-JammaPi.sh
+++ RetroPie/retropiemenu/JammaPi/Attiva ScartPi-JammaPi.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo grep '#dtoverlay=vga666-6' /boot/config.txt > /dev/null 2>&1
 if [ $? -eq 0 ] ; then
+
 echo "Il driver Video non è attivo!"
 sleep 4
 echo "Attivo il driver Video!"
@@ -14,13 +15,22 @@ sudo perl -p -i -e 's/#dpi_group=2/dpi_group=2/g' /boot/config.txt
 sudo perl -p -i -e 's/#dpi_mode=87/dpi_mode=87/g' /boot/config.txt
 sudo perl -p -i -e 's/#hdmi_timings=/hdmi_timings=/g' /boot/config.txt
 
-echo "Modifiche effettuate! Spegnimento in corso!"
-sleep 5
-sudo halt
 else
+
 echo "Il driver video è già attivo!"
+sleep 4
+echo "Disattivo il driver video!"
+
+sudo perl -p -i -e 's/dtoverlay=pwm-2chan,pin=18,func=2,pin2=19,func2=2/#dtoverlay=pwm-2chan,pin=18,func=2,pin2=19,func2=2/g' /boot/config.txt
+sudo perl -p -i -e 's/dtoverlay=vga666-6/#dtoverlay=vga666-6/g' /boot/config.txt
+sudo perl -p -i -e 's/enable_dpi_lcd=1/#enable_dpi_lcd=1/g' /boot/config.txt
+sudo perl -p -i -e 's/display_default_lcd=1/#display_default_lcd=1/g' /boot/config.txt
+sudo perl -p -i -e 's/dpi_output_format=6/#dpi_output_format=6/g' /boot/config.txt
+sudo perl -p -i -e 's/dpi_group=2/#dpi_group=2/g' /boot/config.txt
+sudo perl -p -i -e 's/dpi_mode=87/#dpi_mode=87/g' /boot/config.txt
+sudo perl -p -i -e 's/hdmi_timings=/#hdmi_timings=/g' /boot/config.txt
 fi
 
-
-
-
+echo "Modifiche effettuate!"
+echo "Le impostazioni verranno attivate al prossimo riavvio!"
+sleep 5
