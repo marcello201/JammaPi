278392c86be116a5b18ce60b930c60a556a37edd
vince87 <vincenzo.bini@gmail.com>
2018-10-18 18:29:18 +0200

    Update JammaPi Joystick OFF.sh

--- RetroPie/retropiemenu/JammaPi/JammaPi Joystick OFF.sh
+++ RetroPie/retropiemenu/JammaPi/JammaPi Joystick OFF.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo grep '#dtparam=i2c_vc=on' /boot/config.txt > /dev/null 2>&1
 if [ $? -eq 0 ] ; then
-echo "Il driver joystick JammaPi non è attivo!"
+echo "Il driver joystick JammaPi non Ã¨ attivo!"
 sleep 4
 else
 echo "Disattivo il driver joystick JammaPi!"
@@ -11,9 +11,10 @@ sudo perl -p -i -e 's/mk_arcade_joystick_rpi/#mk_arcade_joystick_rpi/g' /etc/mod
 
 sudo perl -p -i -e 's/dtparam=i2c_vc=on/#dtparam=i2c_vc=on/g' /boot/config.txt
 
-echo "Modifiche effettuate! Riavvio in corso!"
+sudo modprobe -r mk_arcade_joystick_rpi
+
+echo "Modifiche effettuate!"
 sleep 5
-sudo reboot
 fi
 
 
