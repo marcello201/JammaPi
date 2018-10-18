91775b7fb4257e1bca7fbf4d2cbe5424e7d7929e
vince87 <vincenzo.bini@gmail.com>
2018-10-18 18:46:11 +0200

    Update and rename JammaPi Joystick ON.sh to JammaPi Joystick ON-OFF.sh

--- RetroPie/retropiemenu/JammaPi/JammaPi Joystick ON-OFF.sh
+++ RetroPie/retropiemenu/JammaPi/JammaPi Joystick ON-OFF.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo grep '#dtparam=i2c_vc=on' /boot/config.txt > /dev/null 2>&1
 if [ $? -eq 0 ] ; then
+
 echo "Il driver joystick JammaPi non è attivo!"
 sleep 4
 echo "Attivo il driver joystick JammaPi!"
@@ -13,11 +14,22 @@ sudo perl -p -i -e 's/#dtparam=i2c_vc=on/dtparam=i2c_vc=on/g' /boot/config.txt
 sudo modprobe mk_arcade_joystick_rpi i2c0=0x20,0x21
 
 echo "Modifiche effettuate!"
-sleep 5
+
 else
-echo "Il driver joystick JammaPi è già attivo!"
+
+echo "Il driver joystick JammaPi è attivo!"
+sleep 4
+echo "Disattivo il driver joystick JammaPi!"
+
+sudo perl -p -i -e 's/i2c-dev/#i2c-dev/g' /etc/modules
+sudo perl -p -i -e 's/mk_arcade_joystick_rpi/#mk_arcade_joystick_rpi/g' /etc/modules
+
+sudo perl -p -i -e 's/dtparam=i2c_vc=on/#dtparam=i2c_vc=on/g' /boot/config.txt
+
+sudo modprobe -r mk_arcade_joystick_rpi
+
+echo "Modifiche effettuate!"
+
 fi
 
-
-
-
+sleep 5
