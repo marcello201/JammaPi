c84c508845c75ac73a2359d5be47b15149bf5551
vince87 <vincenzo.bini@gmail.com>
2018-11-24 13:13:14 +0100

    Create Controlli analogici.sh

---
+++ RetroPie/retropiemenu/JammaPi/Controlli analogici.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+sudo grep 'input_l_x_minus_axis = "-0"' /opt/retropie/configs/all/retroarch-joypads/MCP23017 Controller.cfg > /dev/null 2>&1
+if [ $? -eq 0 ] ; then
+
+
+
+sudo grep '#input_l_x_minus_axis = "-0"' /opt/retropie/configs/all/retroarch-joypads/MCP23017 Controller.cfg > /dev/null 2>&1
+if [ $? -eq 0 ] ; then
+
+echo "Conversione non attiva!"
+sleep 4
+echo "Attivo controlli analogici!"
+
+sudo perl -p -i -e 's/#input_l_x_minus_axis = "-0"/input_l_x_minus_axis = "-0"/g' /opt/retropie/configs/all/retroarch-joypads/MCP23017 Controller.cfg
+sudo perl -p -i -e 's/#input_l_x_plus_axis = "-0"/input_l_x_plus_axis = "-0"/g' /opt/retropie/configs/all/retroarch-joypads/MCP23017 Controller.cfg
+sudo perl -p -i -e 's/#input_l_y_minus_axis = "-0"/input_l_y_minus_axis = "-0"/g' /opt/retropie/configs/all/retroarch-joypads/MCP23017 Controller.cfg
+sudo perl -p -i -e 's/#input_l_y_plus_axis = "-0"/input_l_y_plus_axis = "-0"/g' /opt/retropie/configs/all/retroarch-joypads/MCP23017 Controller.cfg
+
+echo "Modifiche effettuate!"
+
+else
+
+echo "Conversione attiva!"
+sleep 4
+echo "Disattivo controlli analogici!"
+
+sudo perl -p -i -e 's/input_l_x_minus_axis = "-0"/#input_l_x_minus_axis = "-0"/g' /opt/retropie/configs/all/retroarch-joypads/MCP23017 Controller.cfg
+sudo perl -p -i -e 's/input_l_x_plus_axis = "-0"/#input_l_x_plus_axis = "-0"/g' /opt/retropie/configs/all/retroarch-joypads/MCP23017 Controller.cfg
+sudo perl -p -i -e 's/input_l_y_minus_axis = "-0"/#input_l_y_minus_axis = "-0"/g' /opt/retropie/configs/all/retroarch-joypads/MCP23017 Controller.cfg
+sudo perl -p -i -e 's/input_l_y_plus_axis = "-0"/#input_l_y_plus_axis = "-0"/g' /opt/retropie/configs/all/retroarch-joypads/MCP23017 Controller.cfg
+
+echo "Modifiche effettuate!"
+
+fi
+
+sleep 5
