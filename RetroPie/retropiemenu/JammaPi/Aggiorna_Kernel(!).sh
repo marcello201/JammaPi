41a4895c1188ea36b8c9c510e8f9eb4e83ce2255
vince87 <vincenzo.bini@gmail.com>
2018-11-27 22:45:52 +0100

    Update Aggiorna_Kernel(!).sh

--- RetroPie/retropiemenu/JammaPi/Aggiorna_Kernel(!).sh
+++ RetroPie/retropiemenu/JammaPi/Aggiorna_Kernel(!).sh
@@ -10,13 +10,13 @@ if [ ! -f /kernel-up ]; then
 printf "Primo step!"
 sudo rpi-update
 sudo touch /kernel-up
-printf "Rilanciare lo script dopo il riavvio!"
+printf "Rilanciare lo script dopo il riavvio! \n"
 sleep 10
 sudo reboot
 else
-printf "Step finale!"
+printf "Step finale! \n"
 sudo rpi-source
-printf "KERNEL AGGIORNATO!"
+printf "KERNEL AGGIORNATO! \n"
 sudo dkms build -m mk_arcade_joystick_rpi -v 0.1.5
 sudo dkms install -m mk_arcade_joystick_rpi -v 0.1.5
 printf "\033[1;31m RICOMPILATO IL DRIVER JAMMAPI PER IL NUOVO KERNEL! \033[0m\n"
