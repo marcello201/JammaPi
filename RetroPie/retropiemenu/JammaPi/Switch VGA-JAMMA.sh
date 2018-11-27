d2b122a8bfc32d176383a2a22fc0d00012047eb7
vince87 <vincenzo.bini@gmail.com>
2018-11-27 15:48:44 +0100

    Update Switch VGA-JAMMA.sh

--- RetroPie/retropiemenu/JammaPi/Switch VGA-JAMMA.sh
+++ RetroPie/retropiemenu/JammaPi/Switch VGA-JAMMA.sh
@@ -9,6 +9,7 @@ sudo perl -p -i -e 's/dpi_mode=87/dpi_mode=9/g' /boot/config.txt
 
 sudo grep '#hdmi_timings=' /boot/config.txt > /dev/null 2>&1
 if [ $? -eq 0 ] ; then
+sudo perl -p -i -e 's/dpi_mode=87/dpi_mode=9/g' /boot/config.txt
 sudo perl -p -i -e 's/hdmi_timings=/#hdmi_timings=/g' /boot/config.txt
 fi
 
