eb60a1be0b43aa29676edea937b1f3acdb7140eb
vince87 <vincenzo.bini@gmail.com>
2018-11-27 22:20:46 +0100

    Update Aggiorna_Kernel(!).sh

--- RetroPie/retropiemenu/JammaPi/Aggiorna_Kernel(!).sh
+++ RetroPie/retropiemenu/JammaPi/Aggiorna_Kernel(!).sh
@@ -2,5 +2,15 @@
 
 if [ ! -f /usr/bin/rpi-source ]; then
     echo "Preparo il sistema base!"
-    sudo apt-get install bc libncurses5-dev
+    sudo apt-get update
+    sudo apt-get install -y bc libncurses5-dev rpi-update
+    sudo wget https://raw.githubusercontent.com/notro/rpi-source/master/rpi-source -O /usr/bin/rpi-source && sudo chmod +x /usr/bin/rpi-source && /usr/bin/rpi-source -q --tag-update
 fi
+if [ ! -f /kernel-up ]; then
+sudo rpi-update
+sudo touch /kernel-up
+sudo reboot
+else
+sudo rpi-source
+fi
+
