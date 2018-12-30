eb4697d17735783ec1d64034aca461ad57c96f06
vince87 <vincenzo.bini@gmail.com>
2018-12-30 19:44:55 +0100

    Update led.sh

--- led.sh
+++ led.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo "fase 1: esporto pin Gpio"
 echo "26" > /sys/class/gpio/export
+sleep 2
+echo "fase 2: setto come output"
 echo "out" > /sys/class/gpio/gpio26/direction
+sleep 2
+echo "fase 3: invio valore"
 echo "0" > /sys/class/gpio/gpio26/value
