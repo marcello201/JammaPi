b6d81e4ba3afb17be68190de83b8da0e10c66ae8
vince87 <vincenzo.bini@gmail.com>
2019-07-15 19:27:50 +0200

    Update Switch VGA-JAMMA.sh

--- RetroPie/retropiemenu/JammaPi/Switch VGA-JAMMA.sh
+++ RetroPie/retropiemenu/JammaPi/Switch VGA-JAMMA.sh
@@ -6,17 +6,10 @@ sleep 4
 echo "Conversione a VGA 31Khz"
 
 sudo perl -p -i -e 's/dpi_mode=87/dpi_mode=9/g' /boot/config.txt
-
-sudo grep '#hdmi_timings=' /boot/config.txt > /dev/null 2>&1
-if [ $? -eq 0 ] ; then
-sudo perl -p -i -e 's/dpi_mode=87/dpi_mode=9/g' /boot/config.txt
 sudo perl -p -i -e 's/hdmi_timings=/#hdmi_timings=/g' /boot/config.txt
-fi
 
 else
 
-sudo grep 'dpi_mode=9' /boot/config.txt > /dev/null 2>&1
-if [ $? -eq 0 ] ; then
 echo "Il driver Video è impostato su VGA!"
 sleep 4
 echo "Conversione a JAMMA 15Khz"
@@ -24,7 +17,6 @@ echo "Conversione a JAMMA 15Khz"
 sudo perl -p -i -e 's/dpi_mode=9/dpi_mode=87/g' /boot/config.txt
 sudo perl -p -i -e 's/#hdmi_timings=/hdmi_timings=/g' /boot/config.txt
 
-fi
 fi
 
 echo "Modifiche effettuate!"
