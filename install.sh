d6376fbf15ea8a5fb5f422649177fc85c97046d0
vince87 <vincenzo.bini@gmail.com>
2019-07-19 21:57:42 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,6 +31,7 @@ chmod +x install.sh
 	else
 	sudo perl -p -i -e 's/#hdmi_force_hotplug/hdmi_force_hotplug/g' /boot/config.txt
 	sudo sh -c "echo 'dtparam=i2c_vc=on' >> /boot/config.txt"
+	sudo sh -c "echo '#audio_pwm_mode=2' >> /boot/config.txt"
 	sudo sh -c "echo '#dtoverlay=pwm-2chan,pin=18,func=2,pin2=19,func2=2' >> /boot/config.txt"
 	sudo sh -c "echo '#disable_audio_dither=1' >> /boot/config.txt"
 	sudo sh -c "echo '#dtoverlay=vga666-6' >> /boot/config.txt"
