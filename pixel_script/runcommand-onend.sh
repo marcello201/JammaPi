e734eab9440316f11e9bc400bd958a479cbfc5d1
vince87 <vincenzo.bini@gmail.com>
2019-08-25 12:49:17 +0200

    Update runcommand-onend.sh

--- pixel_script/runcommand-onend.sh
+++ pixel_script/runcommand-onend.sh
@@ -1,3 +1,3 @@
-vcgencmd hdmi_timings 450 1 50 30 90 270 1 10 1 21 0 0 0 50 0 9600000 1 > /dev/null
+vcgencmd hdmi_timings 480 1 14 45 56 300 1 10 5 5 0 0 0 60 0 9600000 1 > /dev/null
 tvservice -e "DMT 87" > /dev/null
 fbset -depth 8 && fbset -depth 16 -xres 450 -yres 270 > /dev/nul
